c035e677c313c2e96638b25cab2960195ff8ca06
tyagimehak <165370482+tyagimehak@users.noreply.github.com>
2024-10-23 22:30:10 +0530

    Create Multilevel_inheritance.cpp

---
+++ Multilevel_inheritance.cpp
@@ -0,0 +1,35 @@
+// MULTILEVEL INHERITANCE
+#include <iostream>
+using namespace std;
+class Person{
+    char name[30];
+    public:
+    void read(){
+        cout << "Enter name : ";
+        cin >> name;
+    }
+    void display(){
+        cout<<"Name : "<<name<<endl;
+    }
+};
+class Sport : public Person{
+    int m = 50;
+    public:
+    void display(){
+        cout << m;
+    }
+};
+class Student : public Sport{
+    public:
+    void display(){
+    Person :: display();
+    Sport :: display();
+    }
+};
+
+int main(){
+    Student st;
+    st.read();
+    st.display();
+    return 0;
+}
