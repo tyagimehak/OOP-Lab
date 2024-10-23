1441a9c88f8dff61712eb2d844f5cfa114604399
tyagimehak <165370482+tyagimehak@users.noreply.github.com>
2024-10-23 22:30:58 +0530

    Create Multiple_inheritance.cpp

---
+++ Multiple_inheritance.cpp
@@ -0,0 +1,35 @@
+// MULTIPLE INHERITANCE
+#include <iostream>
+using namespace std;
+class Person{
+    char name[30];
+    public:
+    void read(){
+        cout << "Enter name : ";
+        cin >> name;
+    }
+    void display(){
+        cout<<"Name : "<<name<<endl;
+    }
+};
+class Sport{
+    int m = 50;
+    public:
+    void display(){
+        cout << m;
+    }
+};
+class Student : public Person , public Sport{
+    public:
+    void display(){
+    Person :: display();
+    Sport :: display();
+    }
+};
+
+int main(){
+    Student st;
+    st.read();
+    st.display();
+    return 0;
+}
